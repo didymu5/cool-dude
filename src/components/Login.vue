5e05c3ae9c35cc7159ed772a857e616a71cff05c
Thomas X. Wu <thomas.wu@kp.org>
2018-06-15 18:30:40 -0700

    initial commit

---
+++ src/components/Login.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="login-form">
+		<h1>Login</h1>
+		<form action="">
+		<input type="text" placeholder="Email">
+		<input type="text" placeholder="Password">
+		</form>
+	</div>
+</template>
+
+<script>
+import auth from 'firebase/auth'
+console.log(auth)
+
+export default {
+  name: 'login',
+}
+</script>
+
+<style>
+input {
+	padding: 15px 15px;
+	font-size: 1.2em;
+	margin-right: 20px;
+}
+</style>
---
+++ src/fb-config.js
@@ -0,0 +1,7 @@
+const config = {
+	apiKey: "AIzaSyAmj8gy-9z8KE5WLh18gE1yoYYbrpqcxOE",
+	authDomain: "cool-dude-c9f5c.firebaseapp.com",
+	databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
+	projectId: "cool-dude-c9f5c"
+};
+export default config;
